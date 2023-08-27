be3bb7f8d28b2f698a31c1ebcc36cf11eb926b25
elBrøwn <elbrown.jad@gmail.com>
2023-08-27 13:20:22 +0300

    Update Slug.cs

--- Assets/Scripts/Slug.cs
+++ Assets/Scripts/Slug.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slug : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float xOffset = 7f;
+
+    private Vector2 _startPos;
+    private Vector2 _targetPos;
+
+    private int _moveDirection = 1;
+
+    private SpriteRenderer _sprite;
+
+    private void Awake()
     {
-        
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _startPos = transform.position;
+    }
+
+    private void Update()
+    {
+
+        _targetPos = _moveDirection == 1 ? _startPos : new Vector2(_startPos.x - xOffset, _startPos.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, _targetPos, speed * Time.deltaTime);
+
+        if (transform.position.x == _targetPos.x)
+        {
+            _moveDirection *= -1;
+            _sprite.flipX = (_moveDirection == 1);
+        }
     }
 }
