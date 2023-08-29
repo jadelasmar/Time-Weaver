255762c28c1e94431a49a70922dd8545e29bc91c
elBrøwn <elbrown.jad@gmail.com>
2023-08-29 11:36:05 +0300

    Update PlayerCollision2D.cs

--- Assets/Scripts/PlayerCollision2D.cs
+++ Assets/Scripts/PlayerCollision2D.cs
@@ -15,49 +15,44 @@ public class PlayerCollision2D : MonoBehaviour
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _gameManager = GameManager.Instance;
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Fall"))
         {
             gameObject.SetActive(false);
-            other.GetComponent<FadeOut>().FadeAndLoadGameOver();
+            FadeOut.Instance.FadeAndLoadGameOver();
         }
         
-        if (other.gameObject.name == ("End Portal"))
-        {
-            other.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-            _animator.SetBool("Port",true);
-            StartCoroutine(MoveToTarget(other.transform));
-        }
         if (other.gameObject.name == ("Start Portal"))
         {
-            Invoke(nameof(enableMovement),(float)0.5);
-          
+            Invoke(nameof(enableMovement), (float)0.5);
         }
     }
 
     private void enableMovement()
     {
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-         GetComponent<PlayerMovement2D>().enabled = true;
-         GetComponent<CharacterController2D>().enabled = true;
+        GetComponent<PlayerMovement2D>().enabled = true;
+        GetComponent<CharacterController2D>().enabled = true;
     }
-    
-    IEnumerator MoveToTarget(Transform portal) {
 
-        while(Vector2.Distance(transform.position, portal.position) > 0.1f) {
+    IEnumerator MoveToTarget(Transform portal)
+    {
+        Vector3 portalCenter = portal.GetComponent<Collider2D>().bounds.center;
 
-            Vector2 direction = (portal.position - transform.position).normalized;
-            _rigidbody2D.AddForce(direction * 10);
+        while (Vector2.Distance(transform.position, portalCenter) > 0.1f) {
 
+            Vector2 direction = (portalCenter - transform.position).normalized;
+
+            _rigidbody2D.AddForce(direction * 2);
+  
             yield return null;
         }
+
         _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
-        
         GetComponent<PlayerMovement2D>().enabled = false;
         GetComponent<CharacterController2D>().enabled = false;
-        _gameManager.LevelDone();
+        _gameManager.LevelComplete();
     }
-    
 }
